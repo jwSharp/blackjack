26d4d92c1a7d5866e0c3a60152a2dae2809cf64c
jwSharp <jws146@pitt.edu>
2022-11-15 15:21:13 -0500

    Update the Cypress tests.

---
+++ cypress/e2e/01-3.cy.js
@@ -0,0 +1,76 @@
+describe("blackjack home page in class practice activity", () => {
+  describe("the rules page", () => {
+    beforeEach(() => {
+      cy.visit("/blackjack/rules.html");
+    });
+    it("contains the required heading", () => {
+      cy.get("article").find("h1").contains("Rules of Blackjack");
+    });
+    it("contains the required content", () => {
+      cy.get("article").find("p").contains("busting");
+    });
+    it("contains the link to table ", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("Table")
+        .invoke("attr", "href")
+        .should("contain", "./table");
+    });
+    it("contains the link to about", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("About")
+        .invoke("attr", "href")
+        .should("contain", "./index");
+    });
+  });
+
+  describe("the about page", () => {
+    beforeEach(() => {
+      cy.visit("/blackjack/index.html");
+    });
+    it("has had the Rules section removed ", () => {
+      cy.get("section").should("not.contain", "Rules");
+    });
+    it("contains the link to table ", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("Table")
+        .invoke("attr", "href")
+        .should("contain", "./table");
+    });
+    it("contains the link to rules", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("Rules")
+        .invoke("attr", "href")
+        .should("contain", "./rules");
+    });
+  });
+
+  describe("the table page", () => {
+    beforeEach(() => {
+      cy.visit("/blackjack/table.html");
+    });
+    it("contains the link to about ", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("About")
+        .invoke("attr", "href")
+        .should("contain", "./index");
+    });
+    it("contains the link to rules", () => {
+      cy.get("header")
+        .find("nav")
+        .find("li")
+        .contains("Rules")
+        .invoke("attr", "href")
+        .should("contain", "./rules");
+    });
+  });
+});
